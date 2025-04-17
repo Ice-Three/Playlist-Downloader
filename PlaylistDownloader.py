a8fdeda9c594558cdee521f4563df6c96ea733e9
Three <157791319+Ice-Three@users.noreply.github.com>
2025-04-17 22:11:26 +0300

    Added youtube download capability

--- PlaylistDownloader.py
+++ PlaylistDownloader.py
@@ -7,108 +7,141 @@ import re
 client_id="YOUR_CLIENT_ID"
 client_secret="YOUR_CLIENT_SECRET"
 
+
+
 def sanitize_filename(name):
     return re.sub(r'[<>:"/\\|?*]', '', name).strip()
 
+print("\nDo you want to download from Spotify or Youtube?")
+ChoiceOfService = int(input("\nSpotify(1) - Youtube(2) - Exit(0)"))
 
-print("Do you wish to download a playlist or an album?")
-ChoiceOfTask = int(input("\nPlaylist (1) - Album (2) - Exit (0) : "))
 
-if ChoiceOfTask == 1: 
+if ChoiceOfService == 1:
+    print("\nDo you wish to download a playlist or an album?")
+    ChoiceOfTask = int(input("\nPlaylist (1) - Album (2) - Exit (0) : "))
 
-    scope = "playlist-read-private"
+    if ChoiceOfTask == 1: 
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri="http://127.0.0.1:8888/callback",
-        scope=scope
-    ))
+        scope = "playlist-read-private"
 
-    playlists = sp.current_user_playlists()
-    playlist_list = []
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri="http://127.0.0.1:8888/callback",
+            scope=scope
+        ))
 
-    print("Your playlists:")
-    for idx, playlist in enumerate(playlists['items']):
-        playlist_list.append({
-            'name':playlist['name'],
-            'id': playlist['id'],
-            'track_count':playlist['tracks']['total']
-        })
-        print(f"{idx}. {playlist['name']} - {playlist['tracks']['total']} tracks")
+        playlists = sp.current_user_playlists()
+        playlist_list = []
 
-    selected_index = int(input("\nEnter the number of the playlist: "))
+        print("Your playlists:")
+        for idx, playlist in enumerate(playlists['items']):
+            playlist_list.append({
+                'name':playlist['name'],
+                'id': playlist['id'],
+                'track_count':playlist['tracks']['total']
+            })
+            print(f"{idx}. {playlist['name']} - {playlist['tracks']['total']} tracks")
 
-    if 0 <= selected_index < len(playlist_list):
-        selected_playlist = playlist_list[selected_index]
-        print(f"\nFetching Songs from playlist: {selected_playlist['name']}...\n")
-        playlist_tracks = sp.playlist_tracks(selected_playlist['id'])
+        selected_index = int(input("\nEnter the number of the playlist: "))
 
+        if 0 <= selected_index < len(playlist_list):
+            selected_playlist = playlist_list[selected_index]
+            print(f"\nFetching Songs from playlist: {selected_playlist['name']}...\n")
+            playlist_tracks = sp.playlist_tracks(selected_playlist['id'])
+
+            tracks = []
+            for idx, item in enumerate(playlist_tracks['items']):
+                track = item['track']
+                if track:
+                    track_name = track['name']
+                    artist_names = ', '.join([artist['name'] for artist in track ['artists']])
+                    print(f"{idx}. {track_name} by {artist_names}")
+                    tracks.append({'name': track_name, 'artists': artist_names})
+        else:
+            print("Invalid selection!")
+            exit()
+
+
+        playlist_folder = sanitize_filename(selected_playlist['name'])
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key':'FFmpegExtractAudio',
+                'preferredcodec':'mp3',
+                'preferredquality':'320',
+            }],
+            'outtmpl': f'{playlist_folder}/%(title)s.%(ext)s',
+            'paths': {'home': os.path.expanduser('~/Downloads')},
+            'quiet': False,
+            'noplaylist': True,
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            for track in tracks:
+                try:
+                    search_query =f"ytsearch:{track['name']} {track['artists']}"
+                    print(f"\nSearching for: {track['name']} {track['artists']}...")
+
+                    ydl.download([search_query])
+                    print(f"Downloaded: {track['name']}")
+                except Exception as e:
+                    print(f"Error downloading {track['name']}: {str(e)}")
+
+
+    elif ChoiceOfTask == 2:
+        scope = "user-library-read"
+
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri="http://127.0.0.1:8888/callback",
+            scope=scope
+        ))
+
+        album_link = str(input("\nPlease put in the album link: "))
+        album_tracks = sp.album_tracks(album_link)
+        album_name = sp.album(album_link)
+
+        print("\nTracks in the album:")
         tracks = []
-        for idx, item in enumerate(playlist_tracks['items']):
-            track = item['track']
-            if track:
-                track_name = track['name']
-                artist_names = ', '.join([artist['name'] for artist in track ['artists']])
-                print(f"{idx}. {track_name} by {artist_names}")
-                tracks.append({'name': track_name, 'artists': artist_names})
-    else:
-        print("Invalid selection!")
-        exit()
+        for idx, item in enumerate(album_tracks['items']):
+            track = item
+            track_name = track['name']
+            artist_names = ', '.join([artist['name'] for artist in track ['artists']])
+            print(f"{idx}. {track_name} by {artist_names}")
+            tracks.append({'name': track_name, 'artists': artist_names})
 
+        album_folder = sanitize_filename(album_name['name'])
 
-    playlist_folder = sanitize_filename(selected_playlist['name'])
-
-    ydl_opts = {
+        ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key':'FFmpegExtractAudio',
             'preferredcodec':'mp3',
             'preferredquality':'320',
         }],
-        'outtmpl': f'{playlist_folder}/%(title)s.%(ext)s',
+        'outtmpl': f'{album_folder}/%(title)s.%(ext)s',
         'paths': {'home': os.path.expanduser('~/Downloads')},
         'quiet': False,
         'noplaylist': True,
     }
+        with YoutubeDL(ydl_opts) as ydl:
+            for track in tracks:
+                try:
+                    search_query =f"ytsearch:{track['name']} {track['artists']}"
+                    print(f"\nSearching for: {track['name']} {track['artists']}...")
 
-    with YoutubeDL(ydl_opts) as ydl:
-        for track in tracks:
-            try:
-                search_query =f"ytsearch:{track['name']} {track['artists']}"
-                print(f"\nSearching for: {track['name']} {track['artists']}...")
+                    ydl.download([search_query])
+                    print(f"Downloaded: {track['name']}")
+                except Exception as e:
+                    print(f"Error downloading {track['name']}: {str(e)}")
 
-                ydl.download([search_query])
-                print(f"Downloaded: {track['name']}")
-            except Exception as e:
-                print(f"Error downloading {track['name']}: {str(e)}")
+    elif ChoiceOfTask == 0:
+        exit()
 
-
-elif ChoiceOfTask == 2:
-    scope = "user-library-read"
-
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri="http://127.0.0.1:8888/callback",
-        scope=scope
-    ))
-
-    album_link = str(input("\nPlease put in the album link: "))
-    album_tracks = sp.album_tracks(album_link)
-    album_name = sp.album(album_link)
-
-    print("\nTracks in the album:")
-    tracks = []
-    for idx, item in enumerate(album_tracks['items']):
-        track = item
-        track_name = track['name']
-        artist_names = ', '.join([artist['name'] for artist in track ['artists']])
-        print(f"{idx}. {track_name} by {artist_names}")
-        tracks.append({'name': track_name, 'artists': artist_names})
-
-    album_folder = sanitize_filename(album_name['name'])
-
+if ChoiceOfService == 2:
     ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -116,21 +149,29 @@ elif ChoiceOfTask == 2:
         'preferredcodec':'mp3',
         'preferredquality':'320',
     }],
-    'outtmpl': f'{album_folder}/%(title)s.%(ext)s',
     'paths': {'home': os.path.expanduser('~/Downloads')},
     'quiet': False,
-    'noplaylist': True,
 }
-    with YoutubeDL(ydl_opts) as ydl:
-        for track in tracks:
-            try:
-                search_query =f"ytsearch:{track['name']} {track['artists']}"
-                print(f"\nSearching for: {track['name']} {track['artists']}...")
+    link = str(input("Input the youtube link:" ))
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
 
-                ydl.download([search_query])
-                print(f"Downloaded: {track['name']}")
-            except Exception as e:
-                print(f"Error downloading {track['name']}: {str(e)}")
+        if 'entries' in info_dict:
+            playlist_title = info_dict.get('title', 'Untitled Playlist')
+            playlist_folder = sanitize_filename(playlist_title)
 
-elif ChoiceOfTask == 0:
+            ydl_opts['outtmpl'] = f'{playlist_folder}/%(title)s.%(ext)s'
+        
+        else:
+            ydl_opts['outtmpl'] = os.path.expanduser('~Downloads/%(title)s.%(ext)s')
+
+        with YoutubeDL(ydl_opts) as ydl:
+                ydl.download([link])
+    
+    except Exception as e:
+        print(f"An  error occured: {e}")
+
+
+elif ChoiceOfService == 0:
     exit()
